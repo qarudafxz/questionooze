697b8d518918dad62535b7ffe332f69127d040ea
GarudaFX <franfra10j@gmail.com>
2024-01-21 13:19:46 +0800

    chore: changed location of the controllers

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-undef */
 import express from 'express'
 import cors from 'cors'
-import pdf from 'pdf-parse/lib/pdf-parse.js'
-import axios from 'axios'
 import dotenv from 'dotenv'
-import { questionGenerator } from './lib/ai.js'
+import routes from './routes/main.js'
 
 dotenv.config()
 const app = express()
@@ -20,33 +18,6 @@ app.use(
 	})
 )
 
-app.get('/api/pdf-parse', async (req, res) => {
-	const { url } = req.query
-
-	try {
-		const response = await axios.get(url, { responseType: 'arraybuffer' })
-
-		const dataBuffer = Buffer.from(response.data)
-
-		const data = await pdf(dataBuffer)
-		const text = data.text.replace(/\/n/g, '\n')
-
-		return res.status(200).json({ context: text })
-	} catch (err) {
-		console.error(err)
-		res.status(500).send('Internal Server Error')
-	}
-})
-
-app.post('/api/question-generator', async (req, res) => {
-	const { config, context } = req.body
-	try {
-		const response = await questionGenerator(config, context)
-		return res.status(200).json(response)
-	} catch (err) {
-		console.error(err)
-		res.status(500).send('Internal Server Error')
-	}
-})
+app.use('/api', routes)
 
 export default app
---
+++ api/routes/main.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+import express from 'express'
+import pdf from 'pdf-parse/lib/pdf-parse.js'
+import axios from 'axios'
+import { questionGenerator } from '../lib/ai.js'
+
+const router = express.Router()
+
+router.get('/pdf-parse', async (req, res) => {
+	try {
+		const { url } = req.query
+
+		try {
+			const response = await axios.get(url, { responseType: 'arraybuffer' })
+
+			const dataBuffer = Buffer.from(response.data)
+
+			const data = await pdf(dataBuffer)
+			const text = data.text.replace(/\/n/g, '\n')
+
+			return res.status(200).json({ context: text })
+		} catch (err) {
+			console.error(err)
+			res.status(500).send('Internal Server Error')
+		}
+	} catch (err) {
+		console.error(err)
+		res.status(500).send('Internal Server Error')
+	}
+})
+
+router.post('/question-generator', async (req, res) => {
+	try {
+		const { config, context } = req.body
+		try {
+			const response = await questionGenerator(config, context)
+			return res.status(200).json(response)
+		} catch (err) {
+			console.error(err)
+			res.status(500).send('Internal Server Error')
+		}
+	} catch (err) {
+		console.error(err)
+		res.status(500).send('Internal Server Error')
+	}
+})
+
+export default router
